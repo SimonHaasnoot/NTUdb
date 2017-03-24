767ec393ea96120b47106d1bd4f82260ce2ff1a1
SimonHaasnoot <simonhaasnoot@hotmail.com>
2017-03-24 12:48:34 +0100

    Add database connection

--- src/App.java
+++ src/App.java
@@ -1,7 +1,16 @@
+import Connection.Database;
+import Gui.Client;
+
 public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Nathan is salty");
+        System.out.println("Initializing connection...");
+        String username = "cringerxd";
+        String password = "Piskut123";
+        String url = "jdbc:sqlserver://p7db.database.windows.net:1433;database=p7db;";
+        Database.Initialize(url, username, password);
+
+        new Client();
     }
 }
---
+++ src/Connection/Database.java
@@ -0,0 +1,34 @@
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Database {
+
+    private static Database instance = null;
+
+    private Connection connection = null;
+
+
+    public Database() {
+    }
+
+    public static void Initialize(String url, String username, String password){
+
+        if(instance == null)
+        instance = new Database();
+
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            instance.connection = DriverManager.getConnection(url, username, password);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Can't connect to the database.");
+        }
+
+        System.out.println("Connected to database");
+
+
+    }
+}
