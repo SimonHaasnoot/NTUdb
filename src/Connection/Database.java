01951eabcb3e553eaebbd6f587564604b5d3cda4
SimonHaasnoot <simonhaasnoot@hotmail.com>
2017-03-27 10:08:36 +0200

    Add return to connection error

--- src/Connection/Database.java
+++ src/Connection/Database.java
@@ -25,6 +25,7 @@ public class Database {
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Can't connect to the database.");
+            return;
         }
 
         System.out.println("Connected to database");
