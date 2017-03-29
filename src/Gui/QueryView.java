e42345a20f4170401fbc452fdbf45633196b85c4
SimonHaasnoot <simonhaasnoot@hotmail.com>
2017-03-29 14:08:36 +0200

    Add function to type in a query

--- src/Gui/QueryView.java
+++ src/Gui/QueryView.java
@@ -19,25 +19,31 @@ public class QueryView extends JPanel {
     private void Init() {
         GridBagConstraints c = new GridBagConstraints();
 
+        c.insets = new Insets(8, 8, 8, 8);
         JButton b1 = new JButton("Opzegredenen");
         b1.addActionListener(e -> client.RefreshTable("SELECT * FROM opzegreden"));
 
         JButton b2 = new JButton("Medewerkers");
         b2.addActionListener(e -> client.RefreshTable("SELECT * FROM Medewerker"));
 
-        JButton b3 = new JButton("test test");
+        JButton b3 = new JButton("niks");
         b3.addActionListener(e -> client.RefreshTable("SELECT * FROM Medewerker"));
 
-        JButton b4 = new JButton("test test");
+        JButton b4 = new JButton("niks");
         b4.addActionListener(e -> client.RefreshTable("SELECT * FROM Medewerker"));
 
-        JButton b5 = new JButton("test test");
-        b5.addActionListener(e -> client.RefreshTable("SELECT * FROM Medewerker"));
+        JTextArea ta = new JTextArea(3, 16);
+        ta.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        JButton b5 = new JButton("run query");
+        b5.addActionListener(e -> client.RefreshTable(ta.getText()));
+
 
         this.add(b1, c);
         this.add(b2, c);
         this.add(b3, c);
         this.add(b4, c);
+        this.add(ta, c);
         this.add(b5, c);
     }
 
