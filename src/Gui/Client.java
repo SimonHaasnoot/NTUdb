7fa3f0be5a6962ca7a4d5d5c3f53159b1bff2870
SimonHaasnoot <simonhaasnoot@hotmail.com>
2017-03-29 13:48:31 +0200

    Add Querying to client side of app

--- src/Gui/Client.java
+++ src/Gui/Client.java
@@ -1,14 +1,8 @@
 package Gui;
 
-import Connection.Database;
-
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Client {
 
@@ -22,7 +16,7 @@ public class Client {
        client.setLayout(new BorderLayout(8, 8));
 
        tableView = new TableView();
-       queryView = new QueryView();
+       queryView = new QueryView(this);
 
        client.add(tableView, BorderLayout.CENTER);
        client.add(queryView, BorderLayout.SOUTH);
@@ -42,37 +36,12 @@ public class Client {
         return frame;
     }
 
-
-    /**
-     * Method to fill a JTable with the ResultSet of a query
-     * @param query
-     * @param table
-     * @throws SQLException
-     */
-    public void FillTable(String query, JTable table) throws SQLException {
-
-        //create connection to database
-        Connection conn = Database.getConnection();
-
-        //make your query
-        Statement statement = conn.createStatement();
-
-        //get ResultSet after entering your query
-        ResultSet result = statement.executeQuery(query);
-
-        while(table.getRowCount() > 0)
-        {
-            ((DefaultTableModel) table.getModel()).removeRow(0);
-        }
-        int columns = result.getMetaData().getColumnCount();
-        while(result.next())
-        {
-            Object[] row = new Object[columns];
-            for (int i = 1; i <= columns; i++)
-            {
-                row[i - 1] = result.getObject(i);
-            }
-            ((DefaultTableModel) table.getModel()).insertRow(result.getRow()-1,row);
+    public void RefreshTable(String query){
+        try {
+            tableView.UpdateTableView(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Something went wrong with refreshing.");
         }
     }
 
